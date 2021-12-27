ea6259226c0081e2d6b8f7aa06ddc18378f30d31
Ankush <ankushpatel1810@gmail.com>
2021-12-27 20:31:14 +0530

    google sign in and mobile icon fixed

--- app/src/main/java/ninja/cricks/Constant.kt
+++ app/src/main/java/ninja/cricks/Constant.kt
@@ -2,6 +2,7 @@ package ninja.cricks
 
 class Constant {
     companion object {
+
         const val delayApiSeconds = 30000L
         const val contestFragmentDatabaseId = 1000000000L
         const val myContestFragmentDatabaseId = 2000000000L
@@ -11,5 +12,6 @@ class Constant {
         const val myLiveMatchesFragmentDatabaseId = 6000000000L
         const val myCompletedMatchesFragmentDatabaseId = 7000000000L
         const val getMessagesDatabaseId = 8000000000L
+        const val getAllMatcesDatabaseId = 9000000000L
     }
 }
